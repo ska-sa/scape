170b67f98348a799ef493bd66d3e0b6525fbba23
Ludwig Schwardt <ludwig@ska.ac.za>
2007-12-17 20:47:47 +0200

    Fixed a show-stopper bug and removed some debugging info.
    git-svn-id: https://katfs/svnCONRAD/trunk/Code/Systems/xdm/xdmsbe/branches/xdm-release/xdmsbe@5322 44c4bee0-8214-0410-80dd-e81c2e0173bc

--- xdmsbelib/stats.py
+++ xdmsbelib/stats.py
@@ -402,7 +402,7 @@ def robust_mu_sigma(data, axis=0):
 
 ## Determine second-order statistics of periodic (angular or directional) data.
 # Convenience function to return second-order statistics of data along given axis as a MuSigmaArray.
-# This handles periodic variables, which exhibit the problem of wrap-around and there are unsuited for
+# This handles periodic variables, which exhibit the problem of wrap-around and therefore are unsuited for
 # the normal mu_sigma function. The period with which the values repeat can be explicitly specified,
 # otherwise the data is assumed to be radians.
 # The mean is in the range -period/2 ... period/2, and the maximum standard deviation is about period/4.
@@ -425,19 +425,14 @@ def periodic_mu_sigma(data, axis=0, period=2.0*np.pi):
     data = data.copy().transpose(moveAxisToFront)
     # Scale data so that one period becomes 2*pi, the natural period for angles
     scale = 2.0 * np.pi / period
-    print "scale", scale
     data *= scale
-    print "data scaled", data
     # Calculate a "safe" mean on the unit circle
     mu = np.arctan2(np.sin(data).mean(axis=0), np.cos(data).mean(axis=0))
     deltaAng = data - mu
-    print "delta1", deltaAng
-    # Wrap angles into interval -pi ... pi
+    # Wrap angle differences into interval -pi ... pi
     deltaAng = (deltaAng + np.pi) % (2.0 * np.pi) - np.pi
-    print "delta2", deltaAng
     # Calculate variance using standard formula with a second correction term
     sigma2 = (deltaAng ** 2.0).mean(axis=0) - (deltaAng.mean(axis=0) ** 2.0)
-    print "sigma2", sigma2
     # Scale answers back to original data range
     return MuSigmaArray(mu / scale, np.sqrt(sigma2) / scale)
 
