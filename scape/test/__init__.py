1c17aef9c58422addca9b8aeae320fdcded6e860
Ludwig Schwardt <ludwig@ska.ac.za>
2009-05-13 18:22:29 +0200

    Added unit tests for projection routines.
    git-svn-id: https://katfs/svnDS/code/scape/trunk@1175 b56c7a7d-ac47-0410-a905-b6f7de78896a

---
+++ scape/test/__init__.py
@@ -0,0 +1,11 @@
+import unittest
+import test_projection
+
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromModule(test_projection))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
