af20df4c18442249256682cc3d3c74ac7a6020d0
Ludwig Schwardt <ludwig@ska.ac.za>
2009-05-26 20:47:13 +0200

    Added gain estimation routine and the outline of a gain calibration routine.
    git-svn-id: https://katfs/svnDS/code/scape/trunk@1445 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scape/gaincal.py
+++ scape/gaincal.py
@@ -51,7 +51,7 @@ class NoiseDiodeBase(object):
         
         Returns
         -------
-        temp : real array, shape (2, *F*)
+        temp : real array, shape (*F*, 2)
             Noise diode temperature interpolated to the frequencies in *freqs*,
             for X and Y polarisations
         
@@ -64,7 +64,7 @@ class NoiseDiodeBase(object):
             interp_x = fitting.randomise(interp_x, self.table_x[:, 0], self.table_x[:, 1], 'shuffle')
             interp_y = fitting.randomise(interp_y, self.table_y[:, 0], self.table_y[:, 1], 'shuffle')
         # Evaluate the smoothed spectrum at the desired frequencies
-        return np.vstack((interp_x(freqs), interp_y(freqs)))
+        return np.dstack((interp_x(freqs), interp_y(freqs))).squeeze()
 
 #--------------------------------------------------------------------------------------------------
 #--- FUNCTIONS
@@ -134,9 +134,72 @@ def estimate_nd_jumps(dataset, min_samples=10, jump_significance=10.0):
                         nd_jump_power.append(nd_delta)
     return nd_jump_times, nd_jump_power
 
-def calibrate(dataset, gain):
-    pass
+def estimate_gain(dataset, **kwargs):
+    """Estimate gain and relative phase of both polarisations via injected noise.
+    
+    Each successful noise diode transition in the data set is used to estimate
+    the gain and relative phase in the two receiver chains for the X and Y
+    polarisations at the instant of the transition. The X and Y gains are power
+    gains (i.e. the square of the voltage gains found in the Jones matrix), and
+    the phase is that of the Y chain relative to the X chain.
+    
+    Parameters
+    ----------
+    dataset : :class:`dataset.DataSet` object
+        Data set to analyse (converted to coherency form in the process)
+    kwargs : dict, optional
+        Extra keyword arguments are passed to :func:`estimate_nd_jumps`
+    
+    Returns
+    -------
+    timestamps : real array, shape (*T*,)
+        Timestamp of each gain measurement, in seconds-since-Unix-epoch
+    gain_xx : real array, shape (*F*, *T*)
+        Power gain of X chain per channel and measurement, in units of raw/K
+    gain_yy : real array, shape (*F*, *T*)
+        Power gain of Y chain per channel and measurement, in units of raw/K
+    phi : real array, shape (*F*, *T*)
+        Phase of Y relative to X, per channel and measurement, in radians
+    
+    """
+    dataset.convert_to_coherency()
+    nd_jump_times, nd_jump_power = estimate_nd_jumps(dataset, **kwargs)
+    if not nd_jump_times:
+        return np.zeros((0)), np.zeros((len(dataset.freqs), 0)), \
+               np.zeros((len(dataset.freqs), 0)), np.zeros((len(dataset.freqs), 0))
+    timestamps = np.array(nd_jump_times)
+    deltas = np.dstack([p.mu for p in nd_jump_power])
+    temp_nd = dataset.noise_diode_data.temperature(dataset.freqs)
+    gain_xx = deltas[:, 0, :] / temp_nd[:, 0, np.newaxis]
+    gain_yy = deltas[:, 1, :] / temp_nd[:, 1, np.newaxis]
+    phi = -np.arctan2(deltas[:, 3, :], deltas[:, 2, :])
+    return timestamps, gain_xx, gain_yy, stats.minimise_angle_wrap(phi)
 
+def calibrate(dataset, max_degree=1, randomise=False, **kwargs):
+    """Calibrate.
+    
+    Parameters
+    ----------
+    dataset : :class:`dataset.DataSet` object
+        Data set to calibrate (converted to coherency form in the process)
+    randomise : {False, True}, optional
+        True if raw data and noise diode spectrum smoothing should be randomised
+    kwargs : dict, optional
+        Extra keyword arguments are passed to :func:`estimate_nd_jumps`
+    
+    """
+    dataset.convert_to_coherency()
+    nd_jump_times, nd_jump_power = estimate_nd_jumps(dataset, **kwargs)
+    gains = np.dstack([p.mu for p in nd_jump_power])
+    if randomise:
+        gains += np.dstack([p.sigma for p in nd_jump_power]) * np.random.standard_normal(gains.shape)
+    temp_nd = dataset.noise_diode_data.temperature(dataset.freqs, randomise)
+    gains[:, :2, :] /= temp_nd[:, :, np.newaxis]
+    interp = fitting.Independent1DFit(fitting.Polynomial1DFit(max_degree=max_degree), axis=2)
+    interp.fit(np.array(nd_jump_times), gains)
+    for ss in dataset.subscans():
+        smooth_gains = interp(ss.timestamps)
+    
 # plots
 # import glob
 # pl.figure()
