70daa0d8f70e0a8c8d255741abf76b6af504b553
Ludwig Schwardt <ludwig@ska.ac.za>
2009-06-13 22:32:30 +0200

    Fixed unit tests.
    git-svn-id: https://katfs/svnDS/code/scape/trunk@1783 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scape/test/__init__.py
+++ scape/test/__init__.py
@@ -2,14 +2,12 @@
 
 import unittest
 # pylint: disable-msg=W0403
-import test_projection
 import test_stats
 import test_fitting
 
 def suite():
     loader = unittest.TestLoader()
     testsuite = unittest.TestSuite()
-    testsuite.addTests(loader.loadTestsFromModule(test_projection))
     testsuite.addTests(loader.loadTestsFromModule(test_stats))
     testsuite.addTests(loader.loadTestsFromModule(test_fitting))
     return testsuite
