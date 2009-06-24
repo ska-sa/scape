b7405570d226b3dae902d97292a81c8258036250
Ludwig Schwardt <ludwig@ska.ac.za>
2009-06-24 18:25:52 +0200

    Made matplotlib an optional dependency for scape. Only the plots will not work in its absence, while all reduction code will still be fine. Also do basic configuration of logging system on scape import, to ensure that error messages get through to the average/casual user.
    git-svn-id: https://katfs/svnDS/code/scape/trunk@1907 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scape/__init__.py
+++ scape/__init__.py
@@ -1,8 +1,18 @@
 """The Single-dish Continuum Analysis PackagE."""
 
+import logging
+import sys
+
+# Set up basic logging if it hasn't been done yet, in order to display error messages and such
+logger = logging.getLogger("scape")
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format="%(levelname)s: %(message)s")
+
 # Most operations are directed through the data set
 from .dataset import DataSet
 
-# Canned plots
-from .plots import plot_waterfall, plot_compound_scan_in_time, plot_compound_scan_on_target, \
-                   plot_data_set_in_mount_space, plot_measured_beam_pattern
+# Canned plots (ignored if no matplotlib is found)
+try:
+    from .plots import plot_waterfall, plot_compound_scan_in_time, plot_compound_scan_on_target, \
+                       plot_data_set_in_mount_space, plot_measured_beam_pattern
+except ImportError:
+    logger.warn('Plotting routines could not be imported (probably because matplotlib is not installed)')
