68d28cc566b883f6795f6c56f67b9e10f2cd0915
Ludwig Schwardt <ludwig@ska.ac.za>
2009-07-09 08:18:33 +0200

    Bug fix to ACSM flux density extraction.
    git-svn-id: https://katfs/svnDS/code/scape/trunk@2152 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scape/xdmfits.py
+++ scape/xdmfits.py
@@ -121,7 +121,7 @@ def acsm_target_description(target):
             min_freq, max_freq = ref_target._fluxDensity._minFreq_MHz, ref_target._fluxDensity._maxFreq_MHz
             coefs = ref_target._fluxDensity._coefficients
             return "%s, radec %s, %s:%s:%s, %s:%s:%s, (%g %g %s)" % \
-                   list(match.groups()) + [min_freq, max_freq, ' '.join('%g' % c for c in coefs)]
+                   tuple(list(match.groups()) + [min_freq, max_freq, ' '.join('%g' % c for c in coefs)])
     match = re.match(r'(.*)Horizontal\(\((-?\d+), (-?\d+), (-?\d+)\), \((-?\d+), (-?\d+), (-?\d+)\)\)', descr)
     if match:
         return "%s, azel, %s:%s:%s, %s:%s:%s" % match.groups()
