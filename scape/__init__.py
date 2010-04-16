6a80e764090cc1d3dbde0dd99c2431b2f53ad177
Ludwig Schwardt <ludwig@ska.ac.za>
2010-04-16 15:50:02 +0200

    Added SVN revision numbers as version numbers for scape and katpoint, accessed via scape.__version__ and katpoint.__version__.
    git-svn-id: https://katfs/svnDS/code/scape/trunk@5131 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scape/__init__.py
+++ scape/__init__.py
@@ -1,16 +1,16 @@
 """The Single-dish Continuum Analysis PackagE."""
 
-import logging
-import sys
+import logging as _logging
+import sys as _sys
 
 # Setup library logger, and suppress spurious logger messages via a null handler
-class _NullHandler(logging.Handler):
+class _NullHandler(_logging.Handler):
     def emit(self, record):
         pass
-logger = logging.getLogger("scape")
+logger = _logging.getLogger("scape")
 logger.addHandler(_NullHandler())
 # Set up basic logging if it hasn't been done yet, in order to display error messages and such
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format="%(levelname)s: %(message)s")
+_logging.basicConfig(level=_logging.DEBUG, stream=_sys.stdout, format="%(levelname)s: %(message)s")
 
 # Most operations are directed through the data set
 from .dataset import DataSet
@@ -19,10 +19,20 @@ from .scan import Scan
 
 # Check if matplotlib is present, otherwise skip plotting routines
 try:
-    import matplotlib as mpl
+    import matplotlib as _mpl
 except ImportError:
     logger.warn('Matplotlib was not found - plotting will be disabled')
 else:
     from .plots_canned import plot_xyz, plot_spectrum, plot_waterfall, plot_spectrogram, plot_fringes, \
                               plot_compound_scan_in_time, plot_compound_scan_on_target, \
                               plot_data_set_in_mount_space, plot_measured_beam_pattern
+
+try:
+    import pkg_resources as _pkg_resources
+    dist = _pkg_resources.get_distribution("scape")
+    # ver needs to be a list since tuples in Python <= 2.5 don't have
+    # a .index method.
+    ver = list(dist.parsed_version)
+    __version__ = "r%d" % int(ver[ver.index("*r") + 1])
+except (ImportError, _pkg_resources.DistributionNotFound, ValueError, IndexError, TypeError):
+    __version__ = "unknown"
