Fixed bug where scan object was evaluated on an empty timestamp array, leading to an IndexError on parallactic angle calculation. This occurred because a dataset selection was made based on flags (flagkeep=~nd_on), and the scan happened to contain a single dump which also had the noise diode firing. The fix is to discard scans if they contain no selected timestamps (or the wrong label).
git-svn-id: https://katfs/svnDS/code/scape/trunk@11333 b56c7a7d-ac47-0410-a905-b6f7de78896a

scape/dataset.py
@@ -374,7 +374,8 @@ class DataSet(object):
                             timekeep &= ~flag_data
                         else:
                             timekeep &= flag_data
-                if (labelkeep is None) or (scan.label in labelkeep):
+                # Discard scan if all times are discarded or label is wrong
+                if ((timekeep is None) or np.any(timekeep)) and ((labelkeep is None) or (scan.label in labelkeep)):
                     scanlist.append(scan.select(timekeep, freqkeep, copy))
             if scanlist:
                 compscanlist.append(CompoundScan(scanlist, compscan.target))