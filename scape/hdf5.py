(schwardt, simonr) Fixed long-standing bug in frequency calculation where the RF centre frequency was thought to line up with the start of a channel instead of the middle. As it stands, the channel centre frequencies are already lined up with the middle of each channel. For example, in the 1024-channel case channel 0 straddles 0 Hz in the baseband, the non-existent channel 1024 straddles 400 MHz and therefore the middle channel 512 straddles 200 MHz exactly, which is where the RF centre frequency is mapped.
git-svn-id: https://katfs/svnDS/code/scape/trunk@14709 b56c7a7d-ac47-0410-a905-b6f7de78896a

scape/hdf5.py
@@ -385,8 +385,8 @@ def load_dataset_v1(filename, baseline='AxAx', selected_pointing='pos_actual_sca
             channel_bw = corrconf_group.attrs['channel_bandwidth_hz'] / 1e6
             num_chans = corrconf_group.attrs['num_freq_channels']
             # Assume that lower-sideband downconversion has been used, which flips frequency axis
-            # Also subtract half a channel width to get frequencies at center of each channel
-            center_freqs = band_center - channel_bw * (np.arange(num_chans) - num_chans / 2 + 0.5)
+            # Don't subtract half a channel width as channel 0 is centred on 0 Hz in baseband
+            center_freqs = band_center - channel_bw * (np.arange(num_chans) - num_chans / 2)
             bandwidths = np.tile(np.float64(channel_bw), num_chans)
         channel_select = corrconf_group['channel_select'].value.nonzero()[0].tolist()
         dump_rate = corrconf_group.attrs['dump_rate_hz']
@@ -692,8 +692,8 @@ def load_dataset_v2(filename, baseline='sd', selected_pointing='pos_actual_scan'
         num_chans = get_single_value(corr_config, 'n_chans')
         channel_bw = get_single_value(corr_config, 'bandwidth') / 1e6 / num_chans
         # Assume that lower-sideband downconversion has been used, which flips frequency axis
-        # Also subtract half a channel width to get frequencies at center of each channel
-        center_freqs = band_center - channel_bw * (np.arange(num_chans) - num_chans / 2 + 0.5)
+        # Don't subtract half a channel width as channel 0 is centred on 0 Hz in baseband
+        center_freqs = band_center - channel_bw * (np.arange(num_chans) - num_chans / 2)
         bandwidths = np.tile(np.float64(channel_bw), num_chans)
         channel_select = range(num_chans)
         sample_period = get_single_value(corr_config, 'int_time')