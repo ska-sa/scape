64f649dc6749770e9d2e0dcbf8940b9fa9914178
Ludwig Schwardt <ludwig@ska.ac.za>
2016-02-15 18:10:30 +0200

    Update URL, keywords and description in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup (
     name = "scape",
-    description = "Karoo Array Telescope common astronomical library'",
+    description = "Karoo Array Telescope single-dish analysis package'",
     author = "Ludwig Schwardt",
     author_email = "ludwig@ska.ac.za",
     packages = find_packages(),
-    url='http://ska.ac.za/',
+    url='https://github.com/ska-sa/scape',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -19,10 +19,9 @@ setup (
     ],
     platforms = [ "OS Independent" ],
     install_requires = ['nose',],
-    keywords="kat kat7 ska",
+    keywords="meerkat ska",
     zip_safe = False,
     setup_requires=['katversion'],
     use_katversion=True,
-    # Bitten Test Suite
     test_suite = "nose.collector",
 )
