6712185b5f4ce64aa0291232e939664226451278
Ludwig Schwardt <ludwig@ska.ac.za>
2016-02-15 18:12:42 +0200

    Make setup.py pass PEP8 (no special formatting!)

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup (
-    name = "scape",
-    description = "Karoo Array Telescope single-dish analysis package'",
-    author = "Ludwig Schwardt",
-    author_email = "ludwig@ska.ac.za",
-    packages = find_packages(),
-    url='https://github.com/ska-sa/scape',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: Other/Proprietary License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
-    platforms = [ "OS Independent" ],
-    install_requires = ['nose',],
-    keywords="meerkat ska",
-    zip_safe = False,
-    setup_requires=['katversion'],
-    use_katversion=True,
-    test_suite = "nose.collector",
-)
+setup(name="scape",
+      description="Karoo Array Telescope single-dish analysis package'",
+      author="Ludwig Schwardt",
+      author_email="ludwig@ska.ac.za",
+      packages=find_packages(),
+      url='https://github.com/ska-sa/scape',
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: Other/Proprietary License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Scientific/Engineering :: Astronomy"],
+      platforms=["OS Independent"],
+      keywords="meerkat ska",
+      zip_safe=False,
+      setup_requires=['katversion'],
+      use_katversion=True,
+      test_suite="nose.collector",
+      install_requires=['nose'])
